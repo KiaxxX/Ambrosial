2d0e05231c2f87e3761a94085e4f7a604a3eea11
likaihua <likaihua@soulapp.cn>
2022-04-26 15:26:11 +0800

    更新
    Signed-off-by: likaihua <likaihua@soulapp.cn>

--- Python/Funcs/StrFuncs/StrFuncs.py
+++ Python/Funcs/StrFuncs/StrFuncs.py
@@ -1,4 +1,5 @@
 # code=utf-8
+import re
 
 
 # 不换行print(实用)
@@ -33,6 +34,19 @@ def endwithFunc(p:str, ch):
     else:
         print("[" + ch + "]不是[" + p + "]的结尾!")
 
+def getFeilds(sql):
+    str = sql.replace("\n", "")
+    while str.find("  ") != -1:
+        str = str.replace("  ", " ");
+    res = re.match(r'select (.*) from', str.lower().strip()).group(1).split(",")
+    files = []
+    for r in res:
+        if r.find(" as ") != -1:
+            files.append(r.split(" as ")[1].strip())
+        else:
+            files.append(r.strip())
+    return files
+
 
 if __name__ == '__main__':
     # capitalizeFunc("likaihua") # Likaihua
@@ -40,4 +54,25 @@ if __name__ == '__main__':
     # centerFunc("likaihua")
     # countFunc("likaihua17864307818", 8)
     # endwithFunc("likaihua", "hua")
+
+    sql = """
+        select 
+            song_id, song_name, sum(hot_score) as socre
+        from rpt.rpt_soc_voiceroom_ktv_song_sd 
+        where day>= '{day}'
+        group by song_id, song_name
+        limit 1
+    """
+    bootstrap_servers='realtime-kafka01.soulapp.cn:9092,' \
+                      'realtime-kafka02.soulapp.cn:9092,' \
+                      'realtime-kafka03.soulapp.cn:9092,' \
+                      'realtime-kafka04.soulapp.cn:9092,' \
+                      'realtime-kafka05.soulapp.cn:9092,' \
+                      'realtime-kafka06.soulapp.cn:9092,' \
+                      'realtime-kafka07.soulapp.cn:9092,' \
+                      'realtime-kafka08.soulapp.cn:9092,' \
+                      'realtime-kafka09.soulapp.cn:9092,' \
+                      'realtime-kafka10.soulapp.cn:9092'
+
+
     printLine("===Func Is Over, Bay-bay===")
