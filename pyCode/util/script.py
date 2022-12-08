ff0abe6e0324d4a16deb24b6ad8544ea31dd4ada
likaihua <likaihua@soulapp.cn>
2022-12-08 14:54:31 +0800

    rename
    Signed-off-by: likaihua <likaihua@soulapp.cn>

--- pyCode/util/script.py
+++ pyCode/util/script.py
@@ -24,10 +24,18 @@ def scriptTimeReverse(the_day, key_day=time.strftime("%Y-%m-%d", time.localtime(
 def printScriptTime(the_year, the_mon, the_day):
     print(
         "nohup" + " "
-        + ""  # 填充Script
+        + "bash ${tools_dir}/exec_hql.sh -f  ${root_dir}/dw_etl/dm/soc/dm.dm_soc_voiceroom_user_sd.hql -t"  # 填充Script
         + " " + the_day + " " + "&"
     )
 
 
 if __name__ == '__main__':
-    scriptTimeReverse("2021-12-31", "2021-08-01")
+    scriptTimeReverse("2022-11-02")
+    # times = '2022-10-10 01:12:12'
+    # time  = datetime.datetime.strptime(times, '%Y-%m-%d %H:%M:%S')
+    # cday = time.strftime('%Y-%m-%d')
+    # print(cday)
+    # ctime = time.strftime('%H')
+    # print(ctime)
+
+
